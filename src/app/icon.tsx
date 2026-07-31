5dec6bff79418ae6af792dbcb38eaaf34eff8b9f
EduExtreme <edugexp@gmail.com>
2026-07-30 22:10:34 -0400

    Use a lightning bolt symbol for the favicon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -6,37 +6,6 @@ export const contentType = "image/png";
 const PRIMARY = "#e8a33d";
 const BACKGROUND = "#0b0c0f";
 
-function Dot({ left, top, dim }: { left: number; top: number; dim: number }) {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        left,
-        top,
-        width: dim,
-        height: dim,
-        borderRadius: "50%",
-        background: PRIMARY,
-      }}
-    />
-  );
-}
-
-function Bar({ left, top, width, height }: { left: number; top: number; width: number; height: number }) {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        left,
-        top,
-        width,
-        height,
-        background: PRIMARY,
-      }}
-    />
-  );
-}
-
 export default function Icon() {
   return new ImageResponse(
     (
@@ -45,20 +14,18 @@ export default function Icon() {
           width: "100%",
           height: "100%",
           display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
           background: BACKGROUND,
           borderRadius: 7,
-          position: "relative",
         }}
       >
-        <Bar left={15} top={8} width={2} height={5} />
-        <Bar left={15} top={19} width={2} height={5} />
-        <Bar left={19} top={15} width={5} height={2} />
-        <Bar left={8} top={15} width={5} height={2} />
-        <Dot left={13} top={2} dim={6} />
-        <Dot left={13} top={24} dim={6} />
-        <Dot left={24} top={13} dim={6} />
-        <Dot left={2} top={13} dim={6} />
-        <Dot left={13} top={13} dim={6} />
+        <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
+          <path
+            d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z"
+            fill={PRIMARY}
+          />
+        </svg>
       </div>
     ),
     { ...size },
